78fbdf085bed4bba0ce1a0fbb8d270b1bfaa874f
djlemmor <lemmornuique@gmail.com>
2021-09-22 20:22:05 +0800

    first commit

---
+++ vue-notes.js
@@ -0,0 +1,21 @@
+// VUE 3 NOTES //
+/*
+
+first install nodejs so we can use node package manager or npm to install vue cli
+type node -v on cmd or any command line to check if we installed nodejs properly
+npm install -g @vue/cli //command to instal vue cli// npm is the nodejs we install // -g means install the package globally
+change folder or directory to where we gonna create our project using cd command in the command line //example cd .. or cd Documents
+vue create project-name // command to create the project // vue is the command from vue cli package we installed 
+
+
+
+
+
+
+
+
+
+
+
+
+*/
