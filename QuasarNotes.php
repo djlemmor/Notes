f92ccf08ed1db92d03b18cc9a7d3e1a446a5739d
djlemmor <lemmornuique@gmail.com>
2021-11-16 19:31:08 +0800

    quasar

---
+++ QuasarNotes.php
@@ -0,0 +1,26 @@
+<?php
+
+// HOW TO START //
+# Node.js >=12.22.1 is required.
+$ yarn global add @quasar/cli
+# or
+$ npm install -g @quasar/cli
+# first install quasar cli
+
+// HOW TO CREATE //
+quasar create <folder_name>
+# make sure you are in your project folder
+quasar dev
+# to run or serve the app
+
+
+// HOW TO CREATE LAYOUT //
+go to quasar Layout and Grid then Layout then Layout Builder
+
+
+
+
+
+
+
+
